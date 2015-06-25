5452f5ef894a0027336f30ca54059a0e3633db38
Giang Le Ngoc <giangln@seesaa.vn>
2015-06-25 11:33:40 +0700

    Initial Commit

---
+++ chatme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chatme
+//
+//  Created by Le Ngoc Giang on 6/25/15.
+//  Copyright (c) 2015 giangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chatme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chatme
+//
+//  Created by Le Ngoc Giang on 6/25/15.
+//  Copyright (c) 2015 giangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
